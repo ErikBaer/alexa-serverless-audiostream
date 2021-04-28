a8cae5e88ec129f61da352bb07b48fa3967e8444
baerdata <baerdata.ger@googlemail.com>
2021-04-28 22:45:01 +0200

    clean up code

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
 import * as Alexa from 'ask-sdk';
 import { RequestHandler, HandlerInput, ErrorHandler, PersistenceAdapter } from 'ask-sdk-core';
 import { DynamoDbPersistenceAdapter } from 'ask-sdk-dynamodb-persistence-adapter'
@@ -27,54 +24,49 @@ const LaunchRequestHandler: RequestHandler = {
             'stream-1',
              0,
              null)
-            // .reprompt(speakOutput)
-             .withShouldEndSession(true)
             .getResponse();
     }
 };
+
 const HelloWorldIntentHandler: RequestHandler = {
     canHandle(handlerInput: HandlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput: HandlerInput) {
-        const speakOutput = 'Hello World, this is baer data!';
+        const speakOutput = 'Hallo, schön das es dich gibt!';
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            .reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
+
 const HelpIntentHandler = {
     canHandle(handlerInput: HandlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput: HandlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
-
+        const speakOutput = 'Hallo, wie kann ich dir helfen?';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
             .getResponse();
     }
 };
+
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput: HandlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && (Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.PauseIntent" ||
-            Alexa.getIntentName(handlerInput.requestEnvelope) ===
-                    "AMAZON.CancelIntent" ||
-                    Alexa.getIntentName(handlerInput.requestEnvelope) ===
-                      "AMAZON.StopIntent")
-
+                Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.CancelIntent" ||
+                Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.StopIntent")
     },
     handle(handlerInput: HandlerInput) {
         const speakOutput = 'Tschüss und bis zum nächsten mal';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .addAudioPlayerStopDirective()
-            //.withShouldEndSession(true)
             .getResponse();
     }
 };
@@ -84,40 +76,17 @@ const SessionEndedRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
     },
     handle(handlerInput: HandlerInput) {
-        // Any cleanup logic goes here.
         return handlerInput.responseBuilder.getResponse();
     }
 };
 
-// The intent reflector is used for interaction model testing and debugging.
-// It will simply repeat the intent the user said. You can create custom handlers
-// for your intents by defining them above, then also adding them to the request
-// handler chain below.
-const IntentReflectorHandler = {
-    canHandle(handlerInput: HandlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput: HandlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-
-// Generic error handling to capture any syntax or routing errors. If you receive an error
-// stating the request handler chain is not found, you have not implemented a handler for
-// the intent being invoked or included it in the skill builder below.
 const ErrorHandler: ErrorHandler = {
     canHandle() {
         return true;
     },
     handle(handlerInput: HandlerInput, error: Error) {
         console.log(`~~~~ Error handled: ${error.stack}`);
-        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+        const speakOutput = `Entschuldigung, da ist anscheinend etwas schief gelaufen. Bitte versuche es noch einmal.`;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -126,9 +95,6 @@ const ErrorHandler: ErrorHandler = {
     }
 };
 
-// The SkillBuilder acts as the entry point for your skill, routing all request and response
-// payloads to the handlers above. Make sure any new handlers or interceptors you've
-// defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
@@ -136,7 +102,6 @@ exports.handler = Alexa.SkillBuilders.custom()
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
-        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
     )
     .addErrorHandlers(
         ErrorHandler,
@@ -157,14 +122,14 @@ exports.handler = Alexa.SkillBuilders.custom()
 
 
 
-    //TODO: clean up file, remove comments etc
-    //TODO: make responses german (move to own file ? depends on time/effort)
-    //TODO: move intents to own file (directories: handlers, interceptors, models, adapter))
+    //TODO: move responses to own file ? (depends on time/effort)
+    //TODO: move intents to own file (directories: handlers, models, adapter( / or util? maybe take tut-util/or rather not.look at it for a second))
     //TODO: add eslint (maybe check udacity proj. for reference, or google reference )
 
-    //TODO: Check with Sebastian about desired requirements
-    //TODO: move intents to own file (directories: handlers, interceptors, models, adapter))
     //TODO: Revise audio files (find nice jingles??)
+    //TODO: maybe emphasize responses (Maybe even pick other voice?)
+
+    //TODO: Check with Sebastian about desired requirements
     // QUESTION: Should i publish and he consumes; Or does he want to be able to release himself?
     // QUESTION: Soll der SKill bloss auf deutsch sein, oder möchte er ihn gerne internationalisiert sehen ?
 
