101968e7a3d6cdee028a846a5fcc215afb7829dc
baerdata <baerdata.ger@googlemail.com>
2021-04-29 21:24:00 +0200

    update tsconfig and move conf_vars to json for future flexibility in prod

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import * as Alexa from 'ask-sdk';
-import { RequestHandler, HandlerInput, ErrorHandler, PersistenceAdapter } from 'ask-sdk-core';
 import { DynamoDbPersistenceAdapter } from 'ask-sdk-dynamodb-persistence-adapter'
 import {LoggingRequestInterceptor,
             LoggingResponseInterceptor,
@@ -36,18 +35,17 @@ exports.handler = Alexa.SkillBuilders.custom()
 
 
 
-    //TODO: move responses to own file !
     //TODO: Check if all is typed up!
-    //TODO: rename vars in LaunchRequestHandler; check naming overall! (also rename model file to make it more descriptive & generic at the same time)
-    //TODO: add eslint (maybe check udacity proj. for reference, or google reference )
+    //TODO: rename vars in LaunchRequestHandler; check naming overall! (also rename model file to make it more descriptive & generic at the same tim
+    //TODO: add uris to  publishingInformation.locales.de-DE.largeIconUri etc. in yml
+    //TODO: Add image and phrase to skill invocation
 
+
+    //TODO: Maybe initiate DYNAMODB in sls.yml instead of adapter.*name-routing* Is is reasonable to keep it flexible?
     //TODO: Revise audio files (find nice jingles??)
     //TODO: maybe emphasize responses (Maybe even pick other voice?)
     //TODO: Test/adjust model-samples .. gehts auch ohne ? Sonst nimm gute!
 
     //TODO: Check with Sebastian about desired requirements
-    // QUESTION: Should i publish and he consumes; Or does he want to be able to release himself?
-    // QUESTION: Soll der SKill bloss auf deutsch sein, oder möchte er ihn gerne internationalisiert sehen ?
-    // Question: Effort: move variables of interest into config file ?!? stream e.g.
     // Question/Consideration: Write Docs / Document process / just howToRun ? Reconsider what you have done, right it down anyway to reproduce quality result!
 
