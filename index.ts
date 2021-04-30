4460fb604a6266c7f9dde7f292509a230bdd6880
baerdata <baerdata.ger@googlemail.com>
2021-04-30 16:16:55 +0200

    move vars to prepare for extension and minor refactor

--- index.ts
+++ index.ts
@@ -7,36 +7,37 @@ import {LoggingRequestInterceptor,
             LoadAttributesRequestInterceptor,
             SaveAttributesResponseInterceptor} from './lambda/interceptors'
 
-import {LaunchRequestHandler, HelloWorldIntentHandler, HelpIntentHandler, CancelAndStopIntentHandler,SessionEndedRequestHandler, RequestErrorHandler} from './lambda/request-handlers'
+import {LaunchRequestHandler, CancelAndStopIntentHandler, HelloWorldIntentHandler, SessionEndedRequestHandler, RequestErrorHandler} from './lambda/request-handlers'
 
 const dynamoDbPersistenceAdapter : PersistenceAdapter = new DynamoDbPersistenceAdapter({ tableName : 'usersTable' });
 
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
         CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-    )
+        SessionEndedRequestHandler)
     .addErrorHandlers(
-        RequestErrorHandler,
-    )
+        RequestErrorHandler)
     .addRequestInterceptors(
-            LoadAttributesRequestInterceptor,
-            LoggingRequestInterceptor)
+        LoadAttributesRequestInterceptor,
+        LoggingRequestInterceptor)
     .addResponseInterceptors(
-            LoggingResponseInterceptor,
-            SaveAttributesResponseInterceptor)
+        LoggingResponseInterceptor,
+        SaveAttributesResponseInterceptor)
     .withPersistenceAdapter(
-             dynamoDbPersistenceAdapter)
+         dynamoDbPersistenceAdapter)
     .lambda();
 
 
     //TODO: add uris to  publishingInformation.locales.de-DE.largeIconUri etc. in yml
     //TODO: Add image and phrase to skill invocation
+    //TODO: Can I have custom intent / skill without helloWorld? Check example / best practice. What intent could i customize?
+    //TODO: npm config
+    //TODO: wright short deploy script which pipes the skill id to env?! maybe; probably yes. npm run etc.
 
     //TODO: Revise audio files (find nice jingles??)
     //TODO: Maybe move IAM to function
+    //TODO: Maybe remove logging interceptors to clean things up (unrequired)
+
     //TODO: Write Docs /   howToRun / whats used / functionality / example usage? Reconsider what you have done, right it down anyway to reproduce quality result!
 
--- lambda/request-handlers.ts
+++ lambda/request-handlers.ts
@@ -9,15 +9,15 @@ const LaunchRequestHandler: RequestHandler = {
     handle(handlerInput: HandlerInput): Response {
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
         const sessionCounter: number = sessionAttributes['sessionCounter'];
-        const speakOutputFirst: string = 'Hallo, das ist die Erstbegrüßung <audio src="soundbank://soundlibrary/aircrafts/futuristic/futuristic_04"/>';
-        const speakOutputLater: string = '<amazon:emotion name="excited" intensity="low">Hallo, das ist die Zweitbegrüßung!</amazon:emotion> <audio src="soundbank://soundlibrary/aircrafts/futuristic/futuristic_11"/>'
+        const speakOutputFirst: string = '<amazon:emotion name="excited" intensity="low"> Hallo, das ist die Erstbegrüßung! </amazon:emotion> <audio src="soundbank://soundlibrary/aircrafts/futuristic/futuristic_04"/>';
+        const speakOutputLater: string = '<amazon:emotion name="excited" intensity="low"> Hallo, das ist die Zweitbegrüßung!</amazon:emotion> <audio src="soundbank://soundlibrary/aircrafts/futuristic/futuristic_11"/>'
         const speakOutput: string = !sessionCounter? speakOutputFirst: speakOutputLater
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .addAudioPlayerPlayDirective(
             'REPLACE_ALL',
-            'https://wdr-wdr2-rheinland.icecastssl.wdr.de/wdr/wdr2/rheinland/mp3/128/stream.mp3',
+            "https://wdr-wdr2-rheinland.icecastssl.wdr.de/wdr/wdr2/rheinland/mp3/128/stream.mp3",
             'baer-data-wrd2-steam-1',
              0,
              undefined)
@@ -31,23 +31,9 @@ const HelloWorldIntentHandler: RequestHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput: HandlerInput): Response {
-        const speakOutput:string = 'Hallo, schön das es dich gibt!';
+        const speakOutput:string = '<amazon:emotion name="excited" intensity="low"> Hallo, schön das es dich gibt! </amazon:emotion>';
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            .getResponse();
-    }
-};
-
-const HelpIntentHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput: HandlerInput): Response {
-        const speakOutput: string = 'Hallo, wie kann ich dir helfen?';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
             .getResponse();
     }
 };
@@ -93,4 +79,4 @@ const RequestErrorHandler: ErrorHandler = {
 };
 
 
-export  {LaunchRequestHandler, HelloWorldIntentHandler, HelpIntentHandler, CancelAndStopIntentHandler, SessionEndedRequestHandler, RequestErrorHandler}
+export  {LaunchRequestHandler, HelloWorldIntentHandler, CancelAndStopIntentHandler, SessionEndedRequestHandler, RequestErrorHandler}
